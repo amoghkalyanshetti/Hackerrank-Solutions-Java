772bcbed2ad5039653a45a8e683b171fc2a45eb9
amogh <amogh@DESKTOP-BD3KMN2>
2018-12-01 23:54:17 +0530

    Added problem statement for ArrayList Practice

--- src/javaPractice/ArrayListPractice.java
+++ src/javaPractice/ArrayListPractice.java
@@ -1,7 +1,6 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+Find the problem statement here
+https://www.hackerrank.com/challenges/java-arraylist/problem
  */
 package javaPractice;
 
